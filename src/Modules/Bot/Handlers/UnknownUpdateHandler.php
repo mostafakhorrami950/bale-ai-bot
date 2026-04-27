bbca6d48c9f05563f888f9bea31c19e299d1f715
Mobix <mostafakhorrami950@gmail.com>
2026-04-27 18:12:03 +0330

    توضیح تغییرات

--- src/Modules/Bot/Handlers/UnknownUpdateHandler.php
+++ src/Modules/Bot/Handlers/UnknownUpdateHandler.php
@@ -1,12 +1,25 @@
 <?php
+
 namespace Modules\Bot\Handlers;
 
-class UnknownUpdateHandler extends BaseHandler {
-    public function handle($update) {
-        // Acknowledge callback to dismiss loading icon on user's side, then do nothing
-        if ($update->isCallback()) {
-            $this->baleClient->answerCallbackQuery($update->getCallbackId(), null, false);
+use Modules\Bot\BaleClient;
+
+class UnknownUpdateHandler extends BaseHandler
+{
+    protected BaleClient $baleClient;
+
+    public function __construct(BaleClient $baleClient)
+    {
+        $this->baleClient = $baleClient;
+    }
+
+    public function handle($update): void
+    {
+        if (method_exists($update, 'isCallback') && $update->isCallback()) {
+            $callbackId = method_exists($update, 'getCallbackId') ? $update->getCallbackId() : null;
+            if ($callbackId) {
+                $this->baleClient->answerCallbackQuery($callbackId);
+            }
         }
-        $this->logger->info('UnknownUpdateHandler: unrecognized update type');
     }
 }
